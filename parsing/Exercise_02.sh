eb72b1d03e5cca23be0335179db911b3aa13b798
MOD Administrator <admin@MngEnvMCAP732343.onmicrosoft.com>
2025-08-06 17:48:53 +0700

    case statement & function

---
+++ parsing/Exercise_02.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# This script demontrates function
+
+function log () {
+  # This function sends a message to syslog and to standard output if VERBOSE is true.
+  local MESSAGE="${@}"
+  if [[ "${VERBOSE}" = 'true' ]]
+  then
+      echo "${MESSAGE}"
+  fi
+  logger -t Exercise_02.sh "${MESSAGE}"
+}
+
+backup_file() {
+  # This function creates a backup of a file. Return non-zero status on error.
+  local FILE="${1}"
+
+  # Make sure the file exists.
+  if [[ -f "${FILE}" ]]
+  then
+    local BACKUP_FILE="/var/tmp/$(basename ${FILE}).$(date +%F-%N)"
+    log "Backing up ${FILE} to ${BACKUP_FILe}."
+
+    # The exit stsatus of the function will be the exit status of the cp command.
+    cp -p ${FILE} ${BACKUP_FILE}
+  else
+    # The file does not exist, so return a non-zero exit status.
+    return 1
+  fi
+}
+
+readonly VERBOSE='true'
+log 'Hello!'
+log 'This is fun!'
+
+backup_file '/etc/passwd'
+
+# Make a decision based on the exit status of the function.
+if [[ "${?}" -eq '0' ]]
+then 
+  log 'File backup succeeded!'
+else
+  log 'File backup failed!'
+  exit 1
+fi
